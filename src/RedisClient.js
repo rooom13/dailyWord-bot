0258f22ab234b294a1c7723f64fb92e632d11822
rooom13 <romanreypedrero@gmail.com>
2018-11-16 01:17:33 +0100

    docker-compose

--- src/RedisClient.js
+++ src/RedisClient.js
@@ -3,14 +3,17 @@ const redis = require('redis')
 module.exports = class {
 
     constructor(fakeUsers) {
-        this.client = redis.createClient()
         this.fakeUsers = fakeUsers
-        this.client.on('error', (err) => console.log('redis error', err))
+
+        if (!this.fakeUsers) {
+            this.client = redis.createClient({host: 'redis'})
+            this.client.on('error', (err) => console.log('redis error', err))
+        }
     }
 
     async getAllActiveChatId() {
 
-        if(this.fakeUsers){
+        if (this.fakeUsers) {
             return new Promise(resolve => resolve(['335813769']))
         }
         return new Promise(resolve => {
@@ -21,18 +24,25 @@ module.exports = class {
     }
 
     saveChatId(chatId) {
-        return new Promise(resolve => {
-            this.client.sadd(['users', chatId], (err, res) => {
-                resolve(res)
+
+        if (!this.fakeUsers) {
+
+
+            return new Promise(resolve => {
+                this.client.sadd(['users', chatId], (err, res) => {
+                    resolve(res)
+                });
             });
-        });
+        }
     }
 
     removeChatId(chatId) {
-        return new Promise(resolve => {
-            this.client.srem(['users', chatId], (err, res) => {
-                resolve(res)
+        if (!this.fakeUsers) {
+            return new Promise(resolve => {
+                this.client.srem(['users', chatId], (err, res) => {
+                    resolve(res)
+                });
             });
-        });
+        }
     }
 }
--- src/server.js
+++ src/server.js
@@ -6,8 +6,12 @@ const { TOKEN } = require('./telegramBot_token.json')
 
 const debug = {
     fakeWord: false,
-    fakeUsers: false
+    fakeUsers: false //Fale DB
 }
+
+console.log('DEBUG')
+console.log(debug)
+
 
 const sheetClient = new SpreadSheetClient(debug.fakeWord)
 const telegramBot = new TelegramBot(TOKEN, debug.fakeUsers)
@@ -22,7 +26,7 @@ const getAndSendWord = () => {
 
 
 var CronJob = require('cron').CronJob;
-new CronJob('40 12,18,20 * * 0-6', function () {
+new CronJob('30 10,18,20 * * 0-6', function () {
     console.log(`Word broadcast sent at ${new Date().toString()}`);
     getAndSendWord()
 
