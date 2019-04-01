2869c61fa4eb396cc2b2ccb3e07b4874641a72e9
rooom13 <romanreypadrero@gmail.com>
2019-04-01 13:08:54 +0200

    no debug

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const { TOKEN, TEST_TOKEN } = require('./telegramBot_token.json')
 // Set testBo: true when developing
 const debug = {
     fakeWord: false,
-    testBot: true,
+    testBot: false,
     redisInDifferentHost: process.argv[2] !== 'local'
 
 }
