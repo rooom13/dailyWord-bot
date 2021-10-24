7f62a24e4d381c4e7ea71950dc268e473c4e85ff
Roman <roman.rey@hawk.ai>
2021-10-24 15:06:20 +0200

    use local tests wordbank

--- daily_word_bot/word_bank.py
+++ daily_word_bot/word_bank.py
@@ -17,7 +17,7 @@ class WordBank:
     df: pd.DataFrame = None
     last_updated_at = None
 
-    def __init__(self, local: bool = False, local_path: typing.Union[str] = "resources/word_bank.csv"):
+    def __init__(self, local: bool = False, local_path: typing.Union[str] = "tests/resources/word_bank.csv"):
         self.local = local
         self.local_path = local_path
         self.update()
