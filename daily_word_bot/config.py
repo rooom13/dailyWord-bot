fa9e15b1b30428a6795232da1046fca3427a3691
Román Rey <32899185+rooom13@users.noreply.github.com>
2021-10-24 16:13:10 +0200

    refactor + backup skeleton (#11)
    * refactor + backup skeleton
    
    Co-authored-by: Roman <roman.rey@hawk.ai>

--- daily_word_bot/config.py
+++ daily_word_bot/config.py
@@ -6,20 +6,24 @@ Config = namedtuple("Config", [
     "ADMIN_CHAT_ID",
     "BOT_TOKEN",
     "REDIS_HOST",
-    "WORD_BANK_LOCAL"])
+    "WORD_BANK_LOCAL",
+    "VERSION",
+])
 
 live_config = Config(
     ADMIN_CHAT_ID=os.getenv("ADMIN_CHAT_ID"),
     BOT_TOKEN=os.getenv("BOT_TOKEN"),
     REDIS_HOST="redis",
-    WORD_BANK_LOCAL=False
+    WORD_BANK_LOCAL=False,
+    VERSION=False
 )
 
 test_config = Config(
     ADMIN_CHAT_ID=os.getenv("ADMIN_CHAT_ID"),
     BOT_TOKEN=os.getenv("TEST_BOT_TOKEN"),
     REDIS_HOST="localhost",
-    WORD_BANK_LOCAL=strtobool(os.getenv("WORD_BANK_LOCAL") or "false")
+    WORD_BANK_LOCAL=strtobool(os.getenv("WORD_BANK_LOCAL") or "true"),
+    VERSION="aVersion"
 )
 
 config = live_config if (os.getenv("ENV") or "").lower() == "live" else test_config
---
+++ tests/test_app.py
@@ -0,0 +1,5 @@
+from daily_word_bot.app import App
+
+
+def test_app():
+    App()
