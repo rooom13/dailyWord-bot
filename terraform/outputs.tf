e655730ef20b7505e8535ade51ea7d44fb25ee03
roman.rey@hawk.ai <roman.rey@hawk.ai>
2026-01-04 11:57:25 +0100

    terraform output docs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,10 @@ output "scheduler_cron_expression" {
   value       = aws_cloudwatch_event_rule.scheduler_trigger.schedule_expression
   description = "Cron expression for the scheduler trigger"
 }
+
+output "set_webhook_command" {
+  sensitive = true
+  value = <<-EOT
+    curl -X POST "https://api.telegram.org/bot${var.bot_token}/setWebhook" -H "Content-Type: application/json" -d '{"url": "${aws_lambda_function_url.webhook.function_url}", "drop_pending_updates": true}'
+  EOT
+}
